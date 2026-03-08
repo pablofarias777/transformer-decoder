9049f1ad0735e4c6b152fc0b7726f14e38f551da
Pablo Farias <pablofarias@MacBook-Air-de-Pablo.local>
2026-03-08 16:41:20 -0300

    Finalizando as tarefas da atividade

--- main.py
+++ main.py
@@ -18,3 +18,37 @@ cross_out = cross_attention(encoder_output, decoder_state)
 
 print("\nCross Attention Output Shape:")
 print(cross_out.shape)
+
+def generate_next_token(current_sequence, encoder_out):
+
+    vocab_size = 10000
+
+    logits = np.random.randn(vocab_size)
+
+    exp_logits = np.exp(logits - np.max(logits))
+    probs = exp_logits / np.sum(exp_logits)
+
+    return probs
+
+    print("\nSimulando geração de texto:")
+    
+current_sequence = ["<START>"]
+
+encoder_out = np.random.randn(1,10,512)
+
+vocab = ["O", "rato", "comeu", "queijo", "<EOS>"]
+
+while True:
+
+    probs = generate_next_token(current_sequence, encoder_out)
+
+    next_token_id = np.argmax(probs)
+
+    next_token = vocab[next_token_id % len(vocab)]
+
+    current_sequence.append(next_token)
+
+    print(current_sequence)
+
+    if next_token == "<EOS>":
+        break
